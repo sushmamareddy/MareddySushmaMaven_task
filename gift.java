fd68f40b9a8bb7733c4cd08f3ab06cd6addffe7f
Mareddy Sushma <36449873+sushmamareddy@users.noreply.github.com>
2020-02-07 00:00:29 +0530

    Add files via upload

---
+++ gift.java
@@ -0,0 +1,17 @@
+package gift;
+import choco.chocolate;
+import sweet.sweets;
+public class gift extends chocolate{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sweets s=new sweets();
+		chocolate c=new chocolate();
+		s.sweet1();
+		s.sweet2();
+		c.choco1();
+		c.choco2();
+		System.out.println("total gifts = "+ count);
+	}
+
+}
